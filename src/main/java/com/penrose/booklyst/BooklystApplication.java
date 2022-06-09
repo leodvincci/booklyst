d2f0128eee7fd296025574c9a09886ab2c32db63
Leo D Penrose <lpenrose@depaul.edu>
2022-06-09 12:56:56 -0500

    first commit

---
+++ src/main/java/com/penrose/booklyst/BooklystApplication.java
@@ -0,0 +1,13 @@
+package com.penrose.booklyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklystApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooklystApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/penrose/booklyst/BooklystApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penrose.booklyst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklystApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
